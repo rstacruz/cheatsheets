1e510e9e0a618daaf17dcb81bd321f4936aabb0f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-10-11 09:30:46 +0800

    Support images

--- _js/behaviors/h3-section-list.js
+++ _js/behaviors/h3-section-list.js
@@ -2,14 +2,24 @@
 
 import Isotope from 'isotope-layout/dist/isotope.pkgd.js'
 import onmount from 'onmount'
+import on from 'dom101/on'
+import qsa from 'dom101/query-selector-all'
 
 /*
  * Behavior: Isotope
  */
 
 onmount('[data-js-h3-section-list]', function () {
-  new Isotope(this, {
+  const iso = new Isotope(this, {
     itemSelector: '.h3-section',
     transitionDuration: 0
+  })
+
+  const images = qsa('img', this)
+
+  images.forEach(image => {
+    on(image, 'load', () => {
+      iso.layout()
+    })
   })
 })

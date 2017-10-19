24641e264e5bdf8d64fe36fa1ddb04034a31409d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-10-20 07:38:50 +0800

    Delay Disqus loading

--- _js/behaviors/disqus.js
+++ _js/behaviors/disqus.js
@@ -3,11 +3,20 @@ import injectDisqus from '../helpers/inject_disqus'
 import ready from 'dom101/ready'
 
 /**
+ * Delay disqus by some time. It's at the bottom of the page, there's no need
+ * for it to load fast. This will give more time to load more critical assets.
+ */
+
+const DISQUS_DELAY = 10 * 1000
+
+/**
  * Injects Disqus onto the page.
  */
 
 onmount('[data-js-disqus]', function () {
   const data = JSON.parse(this.getAttribute('data-js-disqus'))
+  const $parent = this.parentNode
+  $parent.setAttribute('hidden', true)
 
   window.disqus_config = function () {
     this.page.url = data.url
@@ -16,6 +25,9 @@ onmount('[data-js-disqus]', function () {
 
   // Disqus takes a while to load, don't do it so eagerly.
   ready(() => {
-    injectDisqus(data.host)
+    setTimeout(() => {
+      injectDisqus(data.host)
+      $parent.removeAttribute('hidden')
+    }, DISQUS_DELAY)
   })
 })
